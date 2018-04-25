b6d6047835b437e679a2d08ae3ec28cb9ee18e2b
Ivan Shishkov <mail-shishkov@yandex.ru>
2018-04-25 18:52:56 +0300

    Added parsing of filename from command line

--- pprint_json.py
+++ pprint_json.py
@@ -1,4 +1,5 @@
 import json
+import argparse
 
 
 def load_data(filepath):
@@ -9,8 +10,25 @@ def pretty_print_json(data):
     pass
 
 
+def parse_command_line_arguments():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        'filename',
+        help='a JSON file for pretty printing',
+        type=str,
+    )
+
+    command_line_arguments = parser.parse_args()
+
+    return command_line_arguments
+
+
 def main():
-    pass
+    command_line_arguments = parse_command_line_arguments()
+
+    filename = command_line_arguments.filename
+
 
 if __name__ == '__main__':
     main()

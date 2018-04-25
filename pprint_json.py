b9c6b45a9454b4ac8a045a2f4c8cfca00e5fc7f2
Ivan Shishkov <mail-shishkov@yandex.ru>
2018-04-25 19:32:28 +0300

    Added pretty-print of json file content

--- pprint_json.py
+++ pprint_json.py
@@ -38,6 +38,8 @@ def main():
     if not decoded_data:
         sys.exit('JSON file not found')
 
+    print(json.dumps(decoded_data, ensure_ascii=False, indent=4))
+
 
 if __name__ == '__main__':
     main()
